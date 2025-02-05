790b1594907614ce3450ce0f4cc828a77206d468
mohamed <mh1351448@gmail.com>
2025-02-05 16:43:16 +0200

    Global Currency Converter

--- index.js
+++ index.js
@@ -1,53 +1,10 @@
-let names = ['D:ahmed', 'D:ali', 'D:mohamed', 'D:gamal', 'D:hanin', 'D:ahmed'];
-let ages = ['18 years old', '19 years old', '20 years old', '21 years old', '22 years old', '23 years old'];
+let dollar = document.getElementById('dollar');
+let pound = document.getElementById('pound');
 
-let container = document.createElement('div');
-document.body.appendChild(container);
-
-container.style.textAlign = 'center';
-container.style.display = 'flex';
-container.style.flexDirection = 'column';
-container.style.alignItems = 'center';
-
-function element(nameText, ageText, parent) {
-    // عناصر الكارت
-    let card = document.createElement('div');
-    let name = document.createElement('h3');
-    let age = document.createElement('p');
-    let img = document.createElement('img');
-
-    // المحتوى
-    name.textContent = nameText;
-    age.textContent = ageText;
-    img.src = 'img/doctor.png';
-
-    // إضافة العناصر للكارت
-    card.appendChild(name);
-    card.appendChild(age);
-    card.appendChild(img);
-    parent.appendChild(card);
-
-    // تنسيق الكارت
-    card.style.width = '200px';
-    card.style.background = '#444';
-    card.style.padding = '10px';
-    card.style.margin = '10px';
-    card.style.display = 'inline-block';
-    card.style.color = '#fff';
-    card.style.borderRadius = '8px';
-
-    img.style.width = '100px';
-    img.style.borderRadius = '50%';
+dollar.onkeyup = function () {
+    pound.value = dollar.value * 50;
 }
 
-// عرض الكروت بحيث كل 3 كروت في صف
-for (let i = 0; i < names.length; i++) {
-    // عند كل 3 كروت، نبدأ صف جديد
-    if (i % 3 === 0) {
-        var row = document.createElement('div');
-        row.style.display = 'flex';
-        row.style.justifyContent = 'center';
-        container.appendChild(row);
-    }
-    element(names[i], ages[i], row);
+pound.onkeyup = function () {
+    dollar.value = pound.value / 50;
 }
